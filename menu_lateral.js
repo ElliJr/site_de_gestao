36bb4886d33c81d578b3ab92222a72d8c7b4e529
Elli junior <ellijuniorsantosdejesus8@gmail.com>
2025-04-16 22:12:27 -0300

    update

--- menu_lateral.js
+++ menu_lateral.js
@@ -1,6 +1,15 @@
-const menuToggle = document.getElementById('menuToggle');
-const sideMenu = document.getElementById('sideMenu');
 
-menuToggle.addEventListener('click', () => {
-  sideMenu.classList.toggle('active');
-});
+  // Alternar tema claro/escuro
+  const btn = document.getElementById('toggleTheme');
+  btn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    document.body.classList.toggle('light');
+  });
+
+  // Alternar menu lateral
+  const menuToggle = document.getElementById('menuToggle');
+  const sideMenu = document.getElementById('sideMenu');
+
+  menuToggle.addEventListener('click', () => {
+    sideMenu.classList.toggle('active');
+  });
